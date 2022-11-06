963734d0d5b1d5054e1b0726e882640b53ae4548
Muhammad Syachrul <syachrulmuhammad@gmail.com>
2022-11-06 09:43:05 +0700

    fix invalid proprty on svg

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -87,7 +87,7 @@ export default function App({ Component, pageProps }: AppProps) {
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              stroke-width="1.5"
+              strokeWidth={1.5}
               stroke="currentColor"
               className="w-6 h-6"
             >
